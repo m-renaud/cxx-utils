62d4e4df6e85792e516af88c117c7fcd0eabc846
Matt Renaud <mrenaud92@gmail.com>
2013-01-11 15:56:08 -0500

    Added implementation of Andrei Alexandrescu's scope_guard

---
+++ scope_guard.hxx
@@ -0,0 +1,62 @@
+//m=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// Implementation of Andrei Alexandrescu's scope guard.
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+#ifndef MRR_UTILITY_SCOPE_GUARD_
+#define MRR_UTILITY_SCOPE_GUARD_
+
+#include <utility>
+
+namespace mrr {
+
+template <typename Func>
+class scope_guard
+{
+private:
+  bool active_;
+  Func function_;
+
+public:
+  scope_guard()                         = delete;
+  scope_guard(scope_guard&)             = delete;
+  scope_guard& operator=(scope_guard&)  = delete;
+  scope_guard& operator=(scope_guard&&) = delete;
+
+  scope_guard(scope_guard&& sg)
+    : active_(sg.active_), function_(std::move(sg.function_))
+  {
+    sg.active_ = false;
+  }
+
+  scope_guard(Func const& func)
+    : active_(true), function_(func)
+  {
+  }
+
+  scope_guard(Func&& func)
+    : active_(true), function_(std::move(func))
+  {
+  }
+
+  ~scope_guard()
+  {
+    if(active_)
+      function_();
+  }
+
+  void dismiss() noexcept
+  {
+    active_ = false;
+  }
+};
+
+
+template <typename Func>
+scope_guard<Func> make_scope_guard(Func&& f)
+{
+  return scope_guard<Func>(std::forward<Func>(f));
+}
+
+} // namespace mrr
+
+#endif // #ifndef MRR_UTILITY_SCOPE_GUARD_
