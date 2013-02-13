f660cf1eb668ff20c9e284ba2d709a6ae8bdc242
Matt Renaud <mrenaud92@gmail.com>
2013-02-12 23:42:15 -0500

    Added state save point and swap to state.hxx

--- state.hxx
+++ state.hxx
@@ -1,6 +1,8 @@
 #ifndef MRR_STATE_HXX_
 #define MRR_STATE_HXX_
 
+#include <chrono>
+
 //m=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 namespace mrr {
@@ -12,7 +14,8 @@ class state
 {
 public:
   using state_type = std::tuple<TS...>;
-
+  using clock_type = std::chrono::high_resolution_clock;
+  using time_point = clock_type::time_point;
 
   state() = default;
   state(state const& ) = default;
@@ -23,24 +26,39 @@ public:
 
   // Create a state from a list of arguments.
   state(TS&&... vars)
-    : s_(std::move(vars)...)
+    : s_(std::move(vars)...), save_point_(clock_type::now())
   {
   }
 
   // Create a state from an appropriate tuple.
   state(state_type const& tup)
-    : s_(tup)
+    : s_(tup), save_point_(clock_type::now())
   {
   }
 
-  // Restore the context
+  // Get the time that the state was saved at.
+  time_point const& save_point()
+  {
+    return save_point_;
+  }
+
+  // Restore the state held in the current object.
   state_type const& restore_state() const
   {
     return s_;
   }
 
+  // Swap current state with another state.
+  void swap(state& other)
+  {
+    using std::swap;
+    swap(s_, other.s_);
+    swap(save_point_, other.save_point_);
+  }
+
 private:
   state_type s_;
+  time_point save_point_;
 };
 
 
